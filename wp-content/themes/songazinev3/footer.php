816e284238fcd3dd93ad6dc71d0cc49f25e77a0d
Colombe IIM <colombeiim@MacBook-Pro-de-colombe.local>
2018-01-14 14:44:39 +0100

    Add Footer

--- wp-content/themes/songazinev3/footer.php
+++ wp-content/themes/songazinev3/footer.php
@@ -1,26 +1,27 @@
 <html>
 <body>
+        <?php wp_footer(); ?>
+        <br>
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 contact">
+                            <a href="contact.php" > Contact </a>
+                        </div>
+                        <div class="col-md-4 contact ">
+                           <a href="equipe.php" > Equipe </a>
+                        </div>
+                        <div class="col-md-4 contact">
+                            <a href="legal.php" > Légal </a>
+                        </div>
+
+                    </div>
+                </div>
+            </footer>
 
 
-        <footer class="text-center">
-            <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
-                <span class="glyphicon glyphicon-chevron-up"></span>
-            </a><br><br>
-            <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" data-toggle="tooltip" title="Visit w3schools">www.w3schools.com</a></p>
-        </footer>
 
 
-		<?php wp_footer(); ?>
-
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
 
 	</body>
 </html>
